1a780f5a503891d93b5b6c15fa85634095429136
Shashank Kumar <shashank00098@gmail.com>
2023-12-24 04:22:11 +0530

    feat: responsive navbar added.folder structure created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.css";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/home/Home";
+const App = () => {
+  return(
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
